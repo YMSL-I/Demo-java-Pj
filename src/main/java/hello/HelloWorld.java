f2c4dd627bfb0b05c973a3d7463a1662ced64c30
rishabh-ymsli <144879702+rishabh-ymsli@users.noreply.github.com>
2023-09-24 13:31:38 +0900

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
-    int a =2;
+    // int a =2;
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
